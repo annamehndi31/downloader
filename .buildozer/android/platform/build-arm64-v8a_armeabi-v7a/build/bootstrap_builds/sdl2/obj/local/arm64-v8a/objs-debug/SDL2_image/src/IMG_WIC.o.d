a7c2ac5916aedff564e7ec9b660fe252f0f6b27f
annamehndi31 <m.prooftech@gmail.com>
2026-05-09 19:29:45 +0500

    remove buildozer cache fix submodule issue

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2_image/src/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-/home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2_image/src/IMG_WIC.o: \
-  /home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/src/IMG_WIC.c
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/dsp/ssim.o.d
+++
@@ -1,8 +0,0 @@
-/home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/dsp/ssim.o: \
-  /home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/ssim.c \
-  /home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h \
-  /home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h:
-
-/home/annamehndi31/downloader_apk/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
